b552c4cfdf83eed7d9540cb4081279b550d87466
dev-miguel-resources <ingenieromiguelch@gmail.com>
2021-03-29 23:19:45 -0300

    creation Header, Footer and anyFoodDishes components

---
+++ src/components/FoodDishes.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+const FoodDishes = () => (
+    <p>Hola soy el FoodDishes!</p>
+);
+
+
+export default FoodDishes;
---
+++ src/components/PredefinedFoodDishes.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+const PredefinedFoodDishes = () => (
+    <p>Hola soy el PredefinedFoodDishes!</p>
+);
+
+
+export default PredefinedFoodDishes;
