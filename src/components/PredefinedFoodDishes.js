568bdcc448e380543057e0fcf2081d0750b747d3
dev-miguel-resources <ingenieromiguelch@gmail.com>
2021-04-01 08:42:56 -0300

    PredefinedFoodDishes, FoodDishes, FoodDish and NoResults finished

--- src/components/PredefinedFoodDishes.js
+++ src/components/PredefinedFoodDishes.js
@@ -1,8 +1,41 @@
-import React from 'react';
+import React from "react";
+import Grid from "@material-ui/core/Grid";
+import Paper from "@material-ui/core/Paper";
+import { predefinedFoodDishes } from "./../constants";
+import ToggleButton from "@material-ui/lab/ToggleButton";
 
-const PredefinedFoodDishes = () => (
-    <p>Hola soy el PredefinedFoodDishes!</p>
-);
+const PredefinedFoodDishes = ({ getData, setHits }) => {
+  const getFoods = (title) => {
+    predefinedFoodDishes.map((foodDish) => {
+      foodDish.selected = foodDish.title === title ? !foodDish.selected : false;
+      return !foodDish.selected
+        ? setHits()
+        : getData(
+            document.querySelector(`#${title}`).value.toLowerCase().trim()
+          );
+    });
+  };
 
+  return (
+    <Grid item xs={12}>
+      <Paper className="paper">
+        {predefinedFoodDishes.map((foodDish) => {
+          const { title, selected } = foodDish;
+          return (
+            <ToggleButton
+              key={title}
+              id={title}
+              value={title}
+              selected={selected}
+              onClick={() => getFoods(title)}
+            >
+              {title}
+            </ToggleButton>
+          );
+        })}
+      </Paper>
+    </Grid>
+  );
+};
 
 export default PredefinedFoodDishes;
